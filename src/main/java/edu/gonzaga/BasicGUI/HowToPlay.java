33bbd360cd5038279da2026eab0e735e31ed74c8
colinandrew1 <colinmcclelland16@gmail.com>
2023-04-30 12:49:40 -0700

    attempting to set background

--- src/main/java/edu/gonzaga/BasicGUI/HowToPlay.java
+++ src/main/java/edu/gonzaga/BasicGUI/HowToPlay.java
@@ -1,9 +1,11 @@
 package edu.gonzaga.BasicGUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 public class HowToPlay extends JFrame implements ActionListener
 {
@@ -12,32 +14,38 @@ public class HowToPlay extends JFrame implements ActionListener
         new HowToPlay();
     }
 
-    JLabel optionsMenuLabel;
+    JLabel howToPlayLabel;
     JButton backButton;
     JTextArea instructionsTextArea;
 
 
     HowToPlay()
     {
-        optionsMenuLabel = new JLabel();
+        howToPlayLabel = new JLabel();
 
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setVisible(true);//this makes the frame visible
         //this.setSize(600,600);//sets x & y dimensions
         this.setTitle("Onitama");//setting title
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//what happens when we close the window
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//what happens when we close the window
         this.setResizable(true);//we can change whether frame may be resized
         this.getContentPane().setBackground(Color.WHITE);//changing color of frame
-        this.setLayout(null);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         ImageIcon image = new ImageIcon("OnitamaLogo.png"); //creates an image icon
         this.setIconImage(image.getImage()); //changes icon of frame
-        this.setLayout(new BorderLayout());
+        this.setLayout(null);
+        ImageIcon imageIcon = new ImageIcon("boardBackground.png");
+        JLabel backgroundLabel = new JLabel(imageIcon);
+        this.setContentPane(backgroundLabel);
+
+        //BufferedImage myImage = ImageIO.read();
+        JFrame myJFrame = new JFrame("boardBackground.png");
+        //myJFrame.setContentPane(new ImagePanel(myImage));
 
 
-        optionsMenuLabel.setBounds(690, 100, 400, 100);
-        optionsMenuLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
-        optionsMenuLabel.setText("How to play");
+        howToPlayLabel.setBounds(690, 100, 400, 100);
+        howToPlayLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
+        howToPlayLabel.setText("How to play");
 
 
         backButton = new JButton();
@@ -49,10 +57,22 @@ public class HowToPlay extends JFrame implements ActionListener
         backButton.setForeground(Color.BLACK);
 
         instructionsTextArea = new JTextArea();
-
-        this.getContentPane();
-        this.add(optionsMenuLabel);
+        instructionsTextArea.setText("mystringtest");
+        instructionsTextArea.setFont(new Font("MV Boli", Font.BOLD, 15));
+        instructionsTextArea.setEditable(false);
+        instructionsTextArea.setPreferredSize(new Dimension(600, 400));
+        instructionsTextArea.setBounds(50, 200, 1430, 800);
+        instructionsTextArea.setLineWrap(true);
+        instructionsTextArea.setWrapStyleWord(true);
+        String instructionString = "You can never make a move that would cause a pawn to move off the board or move onto the same square as one of your own pawns.";
+        instructionString += "If your pawn moves onto a square that is occupied by one of your opponent's pawns, the opponent's pawn is captured and removed from the game. Put it back in the box.";
+        instructionString += "You must actually land on the piece to capture it - moving over or through a square occupied by an opponent's piece does not capture it.";
+        instructionsTextArea.setText(instructionString);
+        this.add(howToPlayLabel);
         this.add(backButton);
+        this.add(instructionsTextArea);
+
+        
 
     }
 
