cf81e92532bbe6a8a14ea61e7f6adc75d4321195
colinandrew1 <colinmcclelland16@gmail.com>
2023-04-30 15:31:43 -0700

    minor changes

--- src/main/java/edu/gonzaga/BasicGUI/NewGame.java
+++ src/main/java/edu/gonzaga/BasicGUI/NewGame.java
@@ -1,32 +1,19 @@
 package edu.gonzaga.BasicGUI;
 
-import edu.gonzaga.Board;
-import edu.gonzaga.Hand;
-import edu.gonzaga.Player;
-
 import javax.swing.*;
-
 import java.awt.*;
 
 
 
 public class NewGame {
-    Hand hand;
-    Player player1;
-    Player player2;
-    Board board;
     String p1color;
     String p2color;
     ImageIcon[] pieceIcons;
 
     public NewGame(String c1, String c2){
-        hand = new Hand(5);
-        player1 = new Player(1,hand.getPlayer1Cards());
-        player2 = new Player(2,hand.getPlayer2Cards());
         p1color = c1;
         p2color = c2;
         makeIconArray();
-        board = new Board(5, hand, pieceIcons);
         BoardUI app = new BoardUI(pieceIcons);    // Create, then run GUI
         app.runGUI();
     }
@@ -43,5 +30,4 @@ public class NewGame {
             pieceIcons[i] = new ImageIcon(pieceIcons[i].getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH));
         }
     }
-
 }
